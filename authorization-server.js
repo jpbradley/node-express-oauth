fb179334fcdba47e7563e41bb4f40faf724b94c0
Jason Bradley <jason.p.bradley@gmail.com>
2022-02-06 17:10:09 -0600

    added authorize route

--- authorization-server.js
+++ authorization-server.js
@@ -53,6 +53,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
 /*
 Your code here
 */
+app.get('/authorize', function(req,res){
+	res.end()
+})
 
 const server = app.listen(config.port, "localhost", function () {
 	var host = server.address().address
